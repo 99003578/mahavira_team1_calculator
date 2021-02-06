6fbd3ca04fff2c47c6116ade1be5165f05c0281b
99003581 <rupadevisri.rachuri@ltts.com>
2021-02-06 16:53:33 +0530

    updated add

--- 3_Implementation/src/addition.c
+++ 3_Implementation/src/addition.c
@@ -3,8 +3,13 @@ void addition(){
     int a,b,sum;
     printf("Enter Input 1:");
     scanf("%d",&a);
+    if(a>32767)
+    {
+        printf("Please enter the number below 32768");
+    }
     printf("Enter Input 1:"); 
     scanf("%d",&b);
+    printf("Please enter the number below 32768");
     sum=a+b;
     printf("The output of addition is:%d",sum);
 }
