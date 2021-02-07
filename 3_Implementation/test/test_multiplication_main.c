8e68274e218ffe601f17de2e61e54c42abcf2c01
99003580 <78539613+99003580@users.noreply.github.com>
2021-02-07 11:38:59 +0530

    Create test_multiplication_main.c
    created multiplication.main.c

---
+++ 3_Implementation/test/test_multiplication_main.c
@@ -0,0 +1,24 @@
+include "unity.h"
+#include "multiplication.h"
+#include "test_multiplication.h"
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+int main(void)
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_multiplication);
+  RUN_TEST(test_zero_one);
+  RUN_TEST(test_zero_negative);
+  RUN_TEST(test_decimal);
+
+
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
