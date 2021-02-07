de11c2ce545348132f4e339881c8506637174456
99003575 <78539650+99003575@users.noreply.github.com>
2021-02-07 14:19:21 +0530

    Update test_factorial_main.c

--- 3_Implementation/test/test_factorial_main.c
+++ 3_Implementation/test/test_factorial_main.c
@@ -1,1 +1,19 @@
+#include "unity.h"
+#include "test_factorial.h"
 
+/* Required by the unity test framework */
+void setUp() {}
+/* Required by the unity test framework */
+void tearDown() {}
+
+int main(void)
+{
+	/* Initiate the Unity Test Framework */
+	UNITY_BEGIN();
+
+    // Check primality function
+    test_factorial();
+
+	/* Close the Unity Test Framework */
+	return UNITY_END();
+}
