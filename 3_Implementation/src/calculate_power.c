c61415dc29734caf379839401dbf0d144fcc0d01
stepin105061 <preethuck8@gmail.com>
2021-03-01 17:40:31 +0530

    Update calculate_power.c

--- 3_Implementation/src/calculate_power.c
+++ 3_Implementation/src/calculate_power.c
@@ -3,23 +3,29 @@
 void calculate_power()
 
 {
-    double Base_Number, exponent;
-    double result = 1;
+    double Base_Number, exponent,result=1;
+    //double  = 1;
     
     printf("Enter a base number: ");
     scanf("%lf", &Base_Number);
     printf("Enter an exponent: ");
     scanf("%lf", &exponent);
-
-    while (exponent != 0)
+     
+     while(exponent != 0)
     {
+    
         result *= Base_Number;
         --exponent;
-
-    printf("Answer = %lf", result);
-    }
-
-     printf("Invalid");
+    }    
+     
+       if (Base_Number==0)
+     {
+         printf("Invalid");
+         exit(0);
+        
     
-}   
+     }
+     printf("Answer = %lf", result);  
+    
+}
     
