6d24a0db200dbcf58e98f1234d91b186b15513c1
99003579 <78539781+99003579@users.noreply.github.com>
2021-03-01 19:58:52 +0530

    Update test_calculate_power.h

--- 3_Implementation/test/test_calculate_power.h
+++ 3_Implementation/test/test_calculate_power.h
@@ -1,6 +1,8 @@
-#ifndef __TEST_IS_PRIME_H__
-#define __TEST_IS_PRIME_H__
+#ifndef __TEST_CALCULATE_POWER_H__
+#define __TEST_CALCULATE_POWER_H__
 
 void test_calculate_power(void);
+void test_zero_one(void);
+void test_negative(void);
 
 #endif
