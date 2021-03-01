8f7beaea3b475365db5f0448b05ecd30e5ecd3e0
99003574 <78539624+99003574@users.noreply.github.com>
2021-03-01 15:09:49 +0530

    Update division.c

--- 3_Implementation/src/division.c
+++ 3_Implementation/src/division.c
@@ -8,11 +8,13 @@ int division()
     scanf("%f", &num1);
     if(num1 == 0){
         printf("0");
+        return 0;
      }
     printf("Enter the divisor : ");
     scanf("%f",  &num2);
     if(num2 == 0){
         printf("infinty\n");
+        return 0;
      }
     quotient = num1 / num2;
     printf("Quotient = %.02f", quotient);
